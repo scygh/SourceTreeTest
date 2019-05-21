daf073543f887fd6416738a42d7f95b9e160c517
scy <1797484636@qq.com>
2019-05-21 09:54:42 +0800

    add feature_3

--- app/src/main/java/com/scy/android/sourcetreetest/MainActivity.java
+++ app/src/main/java/com/scy/android/sourcetreetest/MainActivity.java
@@ -2,9 +2,11 @@ package com.scy.android.sourcetreetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void feature_2(){
-
+        Log.d(TAG, "feature_2: ");
     }
 
     public void feature_3(){
